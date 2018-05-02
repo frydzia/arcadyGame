154982ecf021f4a02ec72979db8d97f37a21639a
Jola Kasperek <jolakasperek@gmail.com>
2018-05-02 16:02:08 +0200

    Setting the Enemy initial location and speed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +17,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    if (this.x > 510) {
+      this.x = -25;
+      this.speed = 100 + Math.floor((Math.random() * 3) + 1);
+    };
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -28,6 +38,10 @@ Enemy.prototype.render = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var allEnemies = [enemy1, enemy2, enemy3];
 // Place the player object in a variable called player
 
 
